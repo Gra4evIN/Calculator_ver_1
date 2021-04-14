639a2d70173ba2644aa36ba4578c7807221728e1
Ivan <yenirotor@gmail.com>
2021-04-14 22:04:32 +0300

    Made a function

--- calcul_ver_1/calc.py
+++ calcul_ver_1/calc.py
@@ -5,27 +5,21 @@ y = int(input("Введите второе число "))
 z = input("Введите операцию(+, *, -, /, возведение в степень ** ) ")
 
 
-if z == '+':
-    result = x + y
-elif z == '*':
-    result = x * y
-elif z == '-':
-    result = x - y
-elif z == '/' and y != 0:
-    result = x / y
-if y == 0:
-    print('Деление на ноль невозможно')
-elif z == '**':
-    result = x ** y
+def calcul_ver_1():
+    if z == '+':
+        result = x + y
+    elif z == '*':
+        result = x * y
+    elif z == '-':
+        result = x - y
+    elif z == '/' and y != 0:
+        result = x / y
+    if y == 0:
+        print('Деление на ноль невозможно')
+    elif z == '**':
+        result = x ** y
 
-print(result)
-
-
-
+    print(result)
 
 
-
-
-
-
-
+calcul_ver_1()
