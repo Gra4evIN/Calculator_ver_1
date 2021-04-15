31a0567ab1016379dc0aac6f3f5dbbf692c652c7
Ivan <yenirotor@gmail.com>
2021-04-15 20:51:48 +0300

    Made a function Added global variable

--- calcul_ver_1/calc.py
+++ calcul_ver_1/calc.py
@@ -1,25 +1,31 @@
 import math
 
-x = int(input("Введите первое число "))
-y = int(input("Введите второе число "))
-z = input("Введите операцию(+, *, -, /, возведение в степень ** ) ")
-
 
 def calcul_ver_1():
+    global result
+    x = int(input("Введите первое число "))
+    y = int(input("Введите второе число "))
+    z = input("Введите операцию(+, *, -, /, возведение в степень ** ) ")
+
     if z == '+':
         result = x + y
     elif z == '*':
         result = x * y
     elif z == '-':
         result = x - y
+    elif z == '**':
+        result = x ** y
     elif z == '/' and y != 0:
         result = x / y
     if y == 0:
-        print('Деление на ноль невозможно')
-    elif z == '**':
-        result = x ** y
+        result = (print('Деление на ноль невозможно'))
 
     print(result)
 
 
 calcul_ver_1()
+
+
+a = 2
+b = 3
+c = a % b
