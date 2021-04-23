828a60e5ec2613fa66431a833b3e3c0731a1828b
ivan-gi <yenirotor@gmail.com>
2021-04-23 16:33:57 +0300

    Закомментил пару строк Удалил ошибки

--- calc.py
+++ calc.py
@@ -1,11 +1,8 @@
-#import math
-def calcul_ver_1(x, y, z):
-
-    global result
+# import math
+def calcul_ver_1():
     x = int(input("Введите первое число "))
     y = int(input("Введите второе число "))
     z = input("Введите операцию(+, *, -, /, возведение в степень ** ) ")
-
 
     if z == '+':
         result = x + y
@@ -17,15 +14,14 @@ def calcul_ver_1(x, y, z):
         result = x ** y
     elif z == '/' and y != 0:
         result = x / y
-    if y == 0:
-        result = (print('Деление на ноль невозможно'))
+    if y == 0 and z == '/':
+        result = 'Деление на ноль невозможно'
 
     print(result)
 
 
-#calcul_ver_1(1, 2, '*')
+calcul_ver_1()
 
-
-#a = 2
-#b = 3
-#c = a % b
+# a = 2
+# b = 3
+# c = a % b
