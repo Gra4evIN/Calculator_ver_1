287ab3658615b1bf12434f7f9bc9b6f85000dfba
Ivan <yenirotor@gmail.com>
2021-07-22 23:07:06 +0300

    Added Tkint. To make GUI.

--- calcul_ver_1/calc.py
+++ calcul_ver_1/calc.py
@@ -1,15 +1,34 @@
-import math
-improt tkinter
+
 from tkinter import *
+from tkinter import ttk
+from decimal import *
+
+
+root = Tk()
+root.title("calcul_ver_2")
+
+buttons = (('7', '8', '9', '/', '4'),
+           ('4', '5', '6', '*', '4'),
+           ('1', '2', '3', '-', '4'),
+           ('0', '.', '=', '+', '4')
+           )
+
+activeStr = ''
+stack = []
 
 def calcul_ver_1():
-    global result
-    x = int(input("Введите первое число "))
-    y = int(input("Введите второе число "))
-    z = input("Введите операцию(+, *, -, /, возведение в степень ** ) ")
+    global stack
+    global label
+    result = 0
+    operand2 = Decimal(stack.pop())
+    operation = stack.pop()
+    operand1 = Decimal(stack.pop())
+#    x = int(input("Введите первое число "))
+#    y = int(input("Введите второе число "))
+#    z = input("Введите операцию(+, *, -, /, возведение в степень ** ) ")
 
-    if z == '+':
-        result = x + y
+    if operation == '+':
+        result = opera + y
     elif z == '*':
         result = x * y
     elif z == '-':
@@ -20,7 +39,7 @@ def calcul_ver_1():
         result = x / y
     if y == 0:
         result = (print('Деление на ноль невозможно'))
-
+    label.configure(text=str(result))
     print(result)
 
 
@@ -30,3 +49,16 @@ calcul_ver_1()
 a = 2
 b = 3
 c = a % b
+
+
+mainframe = ttk.Frame(root, padding="8 8 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+number = StringVar()
+number_entry = ttk.Entry(mainframe, width=7, textvariable=number)
+number_entry.grid(column=2, row=1, sticky=(W))
+
+numbers = StringVar
+
